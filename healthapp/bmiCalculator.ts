e416615ac6f348e65ae8f6f6d7448de1cb96ea3f
Miszkas <smmisza3@gmail.com>
2026-08-13 07:28:45 +0200

    1. Body Mass Index

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,28 @@
+const bmiCalculator = (height: number, weight: number): string => {
+  const heightInM = height / 100;
+
+  const bmi = weight / (heightInM * heightInM);
+
+  switch (true) {
+    case bmi < 16:
+      return "Underweight (Severe thinness)";
+    case bmi >= 16 && bmi < 17:
+      return "Underweight (Moderate thinness)";
+    case bmi >= 17 && bmi < 18.5:
+      return "Underweight (Mild thinness)";
+    case bmi >= 18.5 && bmi < 25:
+      return "Normal range";
+    case bmi >= 25 && bmi < 30:
+      return "Overweight (Pre-obese)";
+    case bmi >= 30 && bmi < 35:
+      return "Obese (Class I)";
+    case bmi >= 35 && bmi < 40:
+      return "Obese (Class II)";
+    case bmi >= 40:
+      return "Obese (Class III)";
+    default:
+      return "Invalid BMI value";
+  }
+};
+
+console.log(bmiCalculator(170, 65));
