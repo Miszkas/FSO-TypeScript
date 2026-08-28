5ca7149ce1313ed5f1a53fd28b41d10eb5693087
Miszkas <smmisza3@gmail.com>
2026-08-28 14:56:25 +0200

    10. Patientor backend, step 2

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
 const app = express();
+
 app.use(express.json());
+
+const requestLogger = (
+  request: express.Request,
+  _response: express.Response,
+  next: express.NextFunction,
+) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 
 app.get("/api/ping", (_req, res) => {
   console.log("pinged");
