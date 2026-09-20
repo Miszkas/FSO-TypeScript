832a4ed420242aab835a9daccc910425b2e34337
Miszkas <smmisza3@gmail.com>
2026-09-20 21:20:16 +0200

    18. Course, part2

--- hello-tsx/src/App.tsx
+++ hello-tsx/src/App.tsx
@@ -2,13 +2,59 @@ const Header = ({courseName}: {courseName: string}) => {
   return <h1>{courseName}</h1>;
 }
 
-const Content = ({courseParts}: {courseParts: {name: string, exerciseCount: number}[]}) => {
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const Part = ({part}: {part: CoursePart}) => {
+  const partStyle = {
+    marginBottom: "30px",
+    lineHeight: "0.5",
+  };
+  
+  switch (part.kind) {
+    case "basic":
+      return (
+        <div style={partStyle}>
+          <p>{part.name} {part.exerciseCount}</p>
+          <p>{part.description}</p>
+        </div>
+      );
+    case "group":
+      return (
+        <div style={partStyle}>
+          <p>{part.name} {part.exerciseCount}</p>
+          <p>Project exercises: {part.groupProjectCount}</p>
+        </div>
+      );
+    case "background":
+      return (
+        <div style={partStyle}>
+          <p>{part.name} {part.exerciseCount}</p>
+          <p>{part.description}</p>
+          <a href={part.backgroundMaterial}>Background Material</a>
+        </div>
+      );
+    case "special":
+      return (
+        <div style={partStyle}>
+          <p>{part.name} {part.exerciseCount}</p>
+          <p>{part.description}</p>
+          <p>Requirements: {part.requirements.join(", ")}</p>
+        </div>
+      );
+    default:
+      return assertNever(part);
+  }
+}
+
+const Content = ({courseParts}: {courseParts: CoursePart[]}) => {
   return (
     <div>
       {courseParts.map((part, index) => (
-        <p key={index}>
-          {part.name} {part.exerciseCount}
-        </p>
+          <Part part={part} key={index}/>
       ))}
     </div>
   )
@@ -22,20 +68,46 @@ const Total = ({totalFunction}: {totalFunction: () => number}) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+  const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is an awesome course part",
+    kind: "basic"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    kind: "group"
+  },
+  {
+    name: "Basics of type Narrowing",
+    exerciseCount: 7,
+    description: "How to go from unknown to string",
+    kind: "basic"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+    kind: "background"
+  },
+  {
+    name: "TypeScript in frontend",
+    exerciseCount: 10,
+    description: "a hard part",
+    kind: "basic",
+  },
+  {
+  name: "Backend development",
+  exerciseCount: 21,
+  description: "Typing the backend",
+  requirements: ["nodejs", "jest"],
+  kind: "special"
+}
+];
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
 
@@ -49,3 +121,31 @@ const App = () => {
 };
 
 export default App;
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[];
+  kind: "special"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
